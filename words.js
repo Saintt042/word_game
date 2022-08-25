258c4dd77a05610d2f4eff3b778488d497c783a4
saintt042 <alinwachukwumichael1@gmail.com>
2022-08-25 12:50:52 +0100

    Initial commit

---
+++ words.js
@@ -0,0 +1,283 @@
+let words = [
+    {
+        word: "subtraction",
+        hint: "The process of subtracting numbers"
+    },
+    {
+        word: "abstraction",
+        hint: "The process of formulating general concepts"
+    },
+    {
+        word: "Abuse",
+        hint: "Cruel or inhumane treatment"
+    },
+    {
+        word: "Ace",
+        hint: "A playing card in a deck having a single pip on its face"
+    },
+    {
+        word: "Aggregate",
+        hint: "A sum total of many heterogeneous things taken together"
+    },
+    {
+        word: "Ambiguity",
+        hint: "Unclearness by virtue of having more than one meaning"
+    },
+    {
+        word: "Analogy",
+        hint: "Drawing a comparison in order to show a similarity"
+    },
+    {
+        word: "Annoy",
+        hint: "Disturb, especially by minor irritations"
+    },
+    {
+        word: "Anonymous",
+        hint: "Having no known name or identity or known source"
+    },
+    {
+        word: "As follows",
+        hint: "What is listed next"
+    },
+    {
+        word: "Aspect",
+        hint: "A characteristic to be considered"
+    },
+    {
+        word: "Assemble",
+        hint: "Create by putting components or members together"
+    },
+    {
+        word: "Associative",
+        hint: "Resulting from bringing ideas together in the imagination"
+    },
+    {
+        word: "Algorithm",
+        hint: "A set of instructions or rules designed to solve a definite problem."
+    },
+    {
+        word: "Program",
+        hint: "Termed as an organized collection of instructions, which when executed perform a specific task or function."
+    },
+
+    {
+        word: "API",
+        hint: "Application Programming Interface (API) is a set of rules, routines, and protocols to build software applications."
+    },
+    {
+        word: "Argument",
+        hint: "A value that is passed into a command or a function."
+    },
+
+    {
+        word: "ASCII",
+        hint: "A standard that assigns letters, numbers and other characters different slots, available in the 8-bit code."
+    },
+
+    {
+        word: "Boolean",
+        hint: "An expression used for creating statements that are either TRUE or FALSE."
+    },
+
+    {
+        word: "Bug",
+        hint: "A general term used to denote an unexpected error or defect in hardware or software, which causes it to malfunction."
+    },
+
+
+    {
+        word: "Char",
+        hint: "Displays unit of information equal to one alphabetic letter or symbol."
+    },
+    {
+        word: "Objects",
+        hint: "Combination of related variables, constants and other data structures which can be selected and manipulated together."
+    },
+    {
+        word: "Class",
+        hint: "In Object-Oriented programming, refers to a set of related objects with common properties."
+    },
+    {
+        word: "Compilation",
+        hint: "A term used to describe a written set of instructions, written using the protocols of a particular language, such as Java, C or Python."
+    },
+    {
+        word: "Conditionals",
+        hint: "Features of programming language, which help the code make a choice and result in either TRUE or FALSE."
+    },
+    {
+        word: "Constants",
+        hint: "A term used to describe a value that does not change throughout the execution of the program, unlike a variable."
+    },
+    {
+        word: "Array",
+        hint: "Lists or groups of similar types of data values that are grouped."
+    },
+    {
+        word: "Declaration",
+        hint: "A  statement that describes a variable, function or any other identifier"
+    },
+    {
+        word: "Expression",
+        hint: "Legal grouping of letters, symbols, and numbers being used to represent the value of one or more variables."
+    },
+    {
+        word: "Framework",
+        hint: "In programming is a foundation with a specified level of complexity that may be altered by the programmer, making use of their code."
+    },
+    {
+        word: "Hardcode",
+        hint: "In programming used to describe code that is not likely to change."
+    },
+    {
+        word: "Loop",
+        hint: "A sequence of instructions that repeat the same process over and over until a condition is met and it receives the order to stop."
+    }, 
+    {
+        word: "Iteration",
+        hint: "A single pass through a set of operations that deal with code."
+    },
+
+    
+    {
+        word: "Keywords",
+        hint: "Reserved words by a programming language or a program as they have special meaning."
+    },
+    {
+        word: "Operand",
+        hint: " A term used to denote the objects which can be manipulated using different operators."
+    },
+    {
+        word: "Operator",
+        hint: "A term used to denote the object which can manipulate different operands."
+    },
+    {
+        word: "Variable",
+        hint: "A location that stores temporary data within a program which can be modified, store and display whenever need."
+    },
+    {
+        word: "Pointer",
+        hint: "A programming variable that contains the address of a location in the memory."
+    },
+    {
+        word: "Package",
+        hint: "An organized module of related interfaces and classes."
+    },
+    {
+        word: "Runtime",
+        hint: "The time-period during which a program is, in fact, running on a computer."
+    },
+
+    {
+        word: "Backend",
+        hint: "Term used for background in programming."
+    },
+    {
+        word: "Frontend",
+        hint: "The user interface of a computer or any device."
+    },
+
+    {
+        word: "Serverside",
+        hint: "Procedures performed on the server"
+    },
+
+    {
+        word: "Source-data",
+        hint: "The key location from which data is used in the program."
+    },
+
+    {
+        word: "Statement",
+        hint: "A single line of code written legally in a programming language that expresses an action to be carried out."
+    },
+
+    {
+        word: "meeting",
+        hint: "Event in which people come together"
+    },
+    {
+        word: "number",
+        hint: "Math symbol used for counting"
+    },
+    {
+        word: "exchange",
+        hint: "The act of trading"
+    },
+    {
+        word: "canvas",
+        hint: "Piece of fabric for oil painting"
+    },
+    {
+        word: "garden",
+        hint: "Space for planting flower and plant"
+    },
+    {
+        word: "position",
+        hint: "Location of someone or something"
+    },
+    {
+        word: "feather",
+        hint: "Hair like outer covering of bird"
+    },
+    {
+        word: "comfort",
+        hint: "A pleasant feeling of relaxation"
+    },
+    {
+        word: "tongue",
+        hint: "The muscular organ of mouth"
+    },
+    {
+        word: "expansion",
+        hint: "The process of increase or grow"
+    },
+    {
+        word: "country",
+        hint: "A politically identified region"
+    },
+    {
+        word: "group",
+        hint: "A number of objects or persons"
+    },
+    {
+        word: "taste",
+        hint: "Ability of tongue to detect flavour"
+    },
+    {
+        word: "store",
+        hint: "Large shop where goods are traded"
+    },
+    {
+        word: "field",
+        hint: "Area of land for farming activities"
+    },
+    {
+        word: "friend",
+        hint: "Person other than a family member"
+    },
+    {
+        word: "pocket",
+        hint: "A bag for carrying small items"
+    },
+    {
+        word: "needle",
+        hint: "A thin and sharp metal pin"
+    },
+    {
+        word: "expert",
+        hint: "Person with extensive knowledge"
+    },
+    {
+        word: "statement",
+        hint: "A declaration of something"
+    },
+    {
+        word: "second",
+        hint: "One-sixtieth of a minute"
+    },
+    {
+        word: "library",
+        hint: "Place containing collection of books"
+    },
+]
